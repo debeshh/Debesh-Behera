b8edbcbea24d1c30f845a7b1beba40ae6c6069fa
Debesh Behera <anshubehera112@gmail.com>
2025-08-22 23:40:04 +0530

    fixed project links

--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -1,4 +1,5 @@
 import { motion } from "motion/react";
+
 const ProjectDetails = ({
   title,
   description,
@@ -15,20 +16,30 @@ const ProjectDetails = ({
         initial={{ opacity: 0, scale: 0.5 }}
         animate={{ opacity: 1, scale: 1 }}
       >
+        {/* Close Button */}
         <button
           onClick={closeModal}
           className="absolute p-2 rounded-sm top-5 right-5 bg-midnight hover:bg-gray-500"
         >
           <img src="assets/close.svg" className="w-6 h-6" />
         </button>
+
+        {/* Project Image */}
         <img src={image} alt={title} className="w-full rounded-t-2xl" />
+
+        {/* Project Details */}
         <div className="p-5">
           <h5 className="mb-2 text-2xl font-bold text-white">{title}</h5>
           <p className="mb-3 font-normal text-neutral-400">{description}</p>
+
           {subDescription.map((subDesc, index) => (
-            <p className="mb-3 font-normal text-neutral-400">{subDesc}</p>
+            <p key={index} className="mb-3 font-normal text-neutral-400">
+              {subDesc}
+            </p>
           ))}
+
           <div className="flex items-center justify-between mt-4">
+            {/* Tags */}
             <div className="flex gap-3">
               {tags.map((tag) => (
                 <img
@@ -39,9 +50,15 @@ const ProjectDetails = ({
                 />
               ))}
             </div>
-            <a className="inline-flex items-center gap-1 font-medium cursor-pointer hover-animation">
-              View Project{""}
-              <img src="assets/arrow-up.svg" className="size-4" href={href} />
+
+            <a
+              href={href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-flex items-center gap-1 font-medium text-blue-400 hover:text-blue-300 transition hover-animation"
+            >
+              View Project
+              <img src="assets/arrow-up.svg" className="size-4" />
             </a>
           </div>
         </div>
